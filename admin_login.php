39a1009cd10a149431534c21d982c419fe3861d6
alexx <ialexash85@gmail.com>
2023-08-31 19:03:26 +0800

    new

--- admin_login.php
+++ admin_login.php
@@ -13,7 +13,6 @@
     <link rel="stylesheet" href="./css/admin_login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-	
 </head>
 <body>
 	<div class="container">
