acc09918cd66c4049d3667526b9e41ec33587b7e
alexx <ialexash85@gmail.com>
2023-09-05 16:19:38 +0800

    Update bcknd_user_homepage.php

--- backend/bcknd_user_homepage.php
+++ backend/bcknd_user_homepage.php
@@ -22,11 +22,6 @@
     function deflt()
     {
         include  "connection.php";
-        
-        $plant_name = "";
-        $plant_image = "";
-        $plant_genus_name = "";
-        $plant_type_details = "";
 
         $query = "select 
                     plant.plant_name, plant_image, plant.plant_genus_name, plant_type.plant_type_details 
@@ -36,6 +31,24 @@
                     plant.plant_type_id = plant_type.plant_type_id"; 
                     
         $exec = mysqli_query($con, $query);
+
+        display($exec);
+    }
+
+    function filterCategory($category)
+    {
+        include  "connection.php";
+
+        $getCategory = "select 
+                    plant.plant_name, plant_image, plant.plant_genus_name, plant_type.plant_type_details 
+                from 
+                    plant inner join plant_type
+                on 
+                    plant.plant_type_id = plant_type.plant_type_id
+                where
+                    plant_category = '$category' "; 
+                    
+        $exec = mysqli_query($con, $getCategory);
 
         display($exec);
     }
@@ -62,57 +75,68 @@
         if(isset($_GET["floweringPlants"]))
         {
             echo"Flowering Plants";
-            deflt();
+            $category = "floweringPlants";
+            filterCategory($category);
         }
         else if(isset($_GET["succulents&cacti"]))
         {
             echo"Succulents & Cacti";
-            deflt();
+            $category = "succulents&cacti";
+            filterCategory($category);
         }
         else if(isset($_GET["ferns"]))
         {
             echo"Ferns";
-            deflt();
+            $category = "ferns";
+            filterCategory($category);
         }
         else if(isset($_GET["climbers"]))
         {
             echo"Climbers";
-            deflt();
+            $category = "climbers";
+            filterCategory($category);
         }
         else if(isset($_GET["fruitBearing"]))
         {
             echo"Fruit-bearing Plants";
-            deflt();
+            $category = "fruitBearing";
+            filterCategory($category);
         }
         else if(isset($_GET["vegetableBearing"]))
         {
             echo"Vegetable-bearing Plants";
-            deflt();
+            $category = "vegetableBearing";
+            filterCategory($category);
         }
         else if(isset($_GET["herbal"]))
         {
             echo"Herbal Plants";
-            deflt();
+            $category = "herbal";
+            filterCategory($category);
         }
         else if(isset($_GET["fungi"]))
         {
             echo"Fungi";
-            deflt();
+            $category = "fungi";
+            filterCategory($category);
         }
         else if(isset($_GET["carnivorous"]))
         {
             echo"Carnivorous Plants";
-            deflt();
+            $category = "carnivorous";
+            filterCategory($category);
         }
         else if(isset($_GET["toxic"]))
         {
             echo"Toxic Plants";
-            deflt();
+            $category = "toxic";
+            filterCategory($category);
         }
         else if(isset($_GET["ornamental"]))
         {
             echo"Ornamental Plants";
-            deflt();
+            $category = "onramental";
+            filterCategory($category);
         }
         else
         {
