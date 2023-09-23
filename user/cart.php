1e7a0ea8630584480111ecc55c296d248ec539dd
bontilaoreymart <90241649+bontilaoreymart@users.noreply.github.com>
2023-09-23 17:47:04 +0800

    Update cart.php

--- user/cart.php
+++ user/cart.php
@@ -3,8 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Sprout</title>
-		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<title>cart</title>
+		<link rel="stylesheet" href="../css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="../css/user_marketplace.css">
 	</head>
 	<body>
@@ -12,7 +12,7 @@
 		    <div class="container">
 		      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 		        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
-		          <img src="images/Sprout_logo_nobg 2.png" class="brand-logo" alt="">
+		          <a href="user_marketplace.php"><img src="../assets/Sprout_logo_nobg 2.png" class="brand-logo" alt=""></a>
 		        </a>
 
 		        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-1">
@@ -30,17 +30,22 @@
 		  </header>
 		<h1 class="page-heading">Market<span style="color:gold;">place</span></h1>
 		<section class="container">
+			<div class="row cart-lists">
 				<div class="col-sm-3 mt-4">
 					<div class="card">
-					  <img src="images/Group 15.jpg">
-					  <div class="card-body">
-					    <h5 class="card-title">Plant Name</h5>
-					    <p class="card-text">Some quick example text to build on the Plant Name and make up the bulk of the card's content.</p>
-					  </div>
+						<a href="#"><img src="../assets/cart-plus.svg" class="cart-plus-icon"></a>
+						<img src="../assets/Group 15.jpg">
+					  	<div class="cart-price">
+					    	<strong class="text-start">Plant Description</strong>
+					    	<span class="text-end"><strong class="text-end">₱</strong>100.00</span>
+						</div>
+					    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				        dolore magna aliqua.</p>
+						<a href="#" class="btn btn-primary">Add to Cart</a>
 					</div>
 				</div>
 			</div>
 		</section>
-		<script src="js/bootstrap.bundle.min.js"></script>	
+		<script src="../js/bootstrap.bundle.min.js"></script>	
 	</body>
 </html>
