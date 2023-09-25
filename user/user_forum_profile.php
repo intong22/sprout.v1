8b868fd6392c4a3816e934355f4a1ec8c4c36c41
Janni <127589371+Janrie123@users.noreply.github.com>
2023-09-24 23:17:10 -1100

    Add files via upload

---
+++ user/user_forum_profile.php
@@ -0,0 +1,604 @@
+<!DOCTYPE html>
+<!-- Created by CodingLab |www.youtube.com/CodingLabYT-->
+<html lang="en" dir="ltr" >
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+    <link rel="website icon" type="png" href="assets\logo.png">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/user_profile.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-bold-rounded/css/uicons-bold-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-straight/css/uicons-regular-straight.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-bold-rounded/css/uicons-bold-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-straight/css/uicons-regular-straight.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-bold-rounded/css/uicons-bold-rounded.css'>
+       <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <title> Responsive Sidebar Menu  | CodingLab </title>
+    <link rel="stylesheet" href="style.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <style>
+        /* Google Font Link */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins" , sans-serif;
+}
+.sidebar{
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 78px;
+  background: #11101D;
+  padding: 6px 14px;
+  z-index: 99;
+  transition: all 0.5s ease;
+}
+.sidebar.open{
+  width: 250px;
+}
+.sidebar .logo-details{
+  height: 60px;
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+.sidebar .logo-details .icon{
+  opacity: 0;
+  transition: all 0.5s ease;
+}
+.sidebar .logo-details .logo_name{
+  color: #fff;
+  font-size: 20px;
+  font-weight: 600;
+  opacity: 0;
+  transition: all 0.5s ease;
+}
+.sidebar.open .logo-details .icon,
+.sidebar.open .logo-details .logo_name{
+  opacity: 1;
+}
+.sidebar .logo-details #btn{
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+  font-size: 22px;
+  transition: all 0.4s ease;
+  font-size: 23px;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.5s ease;
+}
+.sidebar.open .logo-details #btn{
+  text-align: right;
+}
+.sidebar i{
+  color: #fff;
+  height: 60px;
+  min-width: 50px;
+  font-size: 28px;
+  text-align: center;
+  line-height: 60px;
+}
+.sidebar .nav-list{
+  margin-top: 20px;
+  height: 100%;
+}
+.sidebar li{
+  position: relative;
+  margin: 8px 0;
+  list-style: none;
+}
+.sidebar li .tooltip{
+  position: absolute;
+  top: -20px;
+  left: calc(100% + 15px);
+  z-index: 3;
+  background: #fff;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+  padding: 6px 12px;
+  border-radius: 4px;
+  font-size: 15px;
+  font-weight: 400;
+  opacity: 0;
+  white-space: nowrap;
+  pointer-events: none;
+  transition: 0s;
+}
+.sidebar li:hover .tooltip{
+  opacity: 1;
+  pointer-events: auto;
+  transition: all 0.4s ease;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.sidebar.open li .tooltip{
+  display: none;
+}
+.sidebar input{
+  font-size: 15px;
+  color: #FFF;
+  font-weight: 400;
+  outline: none;
+  height: 50px;
+  width: 100%;
+  width: 50px;
+  border: none;
+  border-radius: 12px;
+  transition: all 0.5s ease;
+  background: #1d1b31;
+}
+.sidebar.open input{
+  padding: 0 20px 0 50px;
+  width: 100%;
+}
+.sidebar .bx-search{
+  position: absolute;
+  top: 50%;
+  left: 0;
+  transform: translateY(-50%);
+  font-size: 22px;
+  background: #1d1b31;
+  color: #FFF;
+}
+.sidebar.open .bx-search:hover{
+  background: #1d1b31;
+  color: #FFF;
+}
+.sidebar .bx-search:hover{
+  background: #FFF;
+  color: #11101d;
+}
+.sidebar li a{
+  display: flex;
+  height: 100%;
+  width: 100%;
+  border-radius: 12px;
+  align-items: center;
+  text-decoration: none;
+  transition: all 0.4s ease;
+  background: #11101D;
+}
+.sidebar li a:hover{
+  background: #FFF;
+}
+.sidebar li a .links_name{
+  color: #fff;
+  font-size: 15px;
+  font-weight: 400;
+  white-space: nowrap;
+  opacity: 0;
+  pointer-events: none;
+  transition: 0.4s;
+}
+.sidebar.open li a .links_name{
+  opacity: 1;
+  pointer-events: auto;
+}
+.sidebar li a:hover .links_name,
+.sidebar li a:hover i{
+  transition: all 0.5s ease;
+  color: #11101D;
+}
+.sidebar li i{
+  height: 50px;
+  line-height: 50px;
+  font-size: 18px;
+  border-radius: 12px;
+}
+.sidebar li.profile{
+  position: fixed;
+  height: 60px;
+  width: 78px;
+  left: 0;
+  bottom: -8px;
+  padding: 10px 14px;
+  background: #1d1b31;
+  transition: all 0.5s ease;
+  overflow: hidden;
+}
+.sidebar.open li.profile{
+  width: 250px;
+}
+.sidebar li .profile-details{
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+}
+.sidebar li img{
+  height: 45px;
+  width: 45px;
+  object-fit: cover;
+  border-radius: 6px;
+  margin-right: 10px;
+}
+.sidebar li.profile .name,
+.sidebar li.profile .job{
+  font-size: 15px;
+  font-weight: 400;
+  color: #fff;
+  white-space: nowrap;
+}
+.sidebar li.profile .job{
+  font-size: 12px;
+}
+.sidebar .profile #log_out{
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+  background: #1d1b31;
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  border-radius: 0px;
+  transition: all 0.5s ease;
+}
+.sidebar.open .profile #log_out{
+  width: 50px;
+  background: none;
+}
+.home-section{
+  position: relative;
+  background: #E4E9F7;
+  min-height: 100vh;
+  top: 0;
+  left: 78px;
+  width: calc(100% - 78px);
+  transition: all 0.5s ease;
+  z-index: 2;
+}
+.sidebar.open ~ .home-section{
+  left: 250px;
+  width: calc(100% - 250px);
+}
+.home-section .text{
+  display: inline-block;
+  color: #11101d;
+  font-size: 25px;
+  font-weight: 500;
+  margin: 18px
+}
+@media (max-width: 420px) {
+  .sidebar li .tooltip{
+    display: none;
+  }
+}
+<header class="p-0 mb-3 border-bottom">
+		    <div class="container">
+			    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+			        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+			        </a><a href="user_homepage.php"><img src="../assets/Sprout_logo_nobg 2.png" class="brand-logo" alt=""></a>
+			        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-1">
+			          <li><a href="#" class="nav-link px-2 link-body-emphasis">Homepage</a></li>
+			          <li><a href="#" class="nav-link px-2 link-body-emphasis">Encyclopedia</a></li>
+			          <li><a href="#" class="nav-link px-2 link-body-emphasis">Forum</a></li>
+			          <li><a href="#" class="nav-link px-2 link-body-emphasis">Favorites</a></li>
+			        </ul>
+               <div id="header">
+          <img src="../assets/Sprout_logo_nobg 2.png" class="brand-logo" alt="">
+   
+			        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+			          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+			        </form>
+			        <div>
+			        	<a href="cart.php"><img src="../assets/cart-plus.svg" class="cart4-icon"></a>
+			        </div>
+			    </div>
+		    </div>
+		  </header>
+     </style>
+   </head>
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus icon'></i>
+        <div class="logo_name">Sprout</div>
+        <i class='bx bx-menu' id="btn" ></i>
+    </div>
+    <ul class="nav-list">
+      <li>
+          <i class='bx bx-search' ></i>
+         <input type="text" placeholder="Search...">
+         <span class="tooltip">Search</span>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">HOME</span>
+        </a>
+         <span class="tooltip">HOME</span>
+      </li>
+      <li>
+       <a href="#">
+         <i class='bx bx-user' ></i>
+         <span class="links_name">User</span>
+       </a>
+       <span class="tooltip">User</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-chat' ></i>
+         <span class="links_name">Encyclopedia</span>
+       </a>
+       <span class="tooltip">Encyclopedia</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-pie-chart-alt-2' ></i>
+         <span class="links_name">Community Forum</span>
+       </a>
+       <span class="tooltip">Community Forum</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-folder' ></i>
+         <span class="links_name">Marketplace</span>
+       </a>
+       <span class="tooltip">Marketplace</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-cart-alt' ></i>
+         <span class="links_name">Order</span>
+       </a>
+       <span class="tooltip">Order</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-heart' ></i>
+         <span class="links_name">Saved</span>
+       </a>
+       <span class="tooltip">Saved</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-cog' ></i>
+         <span class="links_name">Setting</span>
+       </a>
+       <span class="tooltip">Setting</span>
+     </li>
+     <li class="profile">
+         <div class="profile-details">
+           <img src="profile.jpg" alt="profileImg">
+           <div class="name_job">
+             <div class="name">Prem Shahi</div>
+             <div class="job">Web designer</div>
+           </div>
+         </div>
+         <i class='bx bx-log-out' id="log_out" ></i>
+     </li>
+    </ul>
+  </div>
+  
+  <script>
+  let sidebar = document.querySelector(".sidebar");
+  let closeBtn = document.querySelector("#btn");
+  let searchBtn = document.querySelector(".bx-search");
+
+  closeBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("open");
+    menuBtnChange();//calling the function(optional)
+  });
+
+  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
+  });
+
+  // following are the code to change sidebar button(optional)
+  function menuBtnChange() {
+   if(sidebar.classList.contains("open")){
+     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   }else {
+     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   }
+  }
+  </script>
+  <div class="grand-parent">
+        <div class="parent">
+           
+            <div class="back">
+                <a href="user_homepage.php"><i class="fi fi-rr-arrow-small-right"></i></a>
+            </div>
+           
+
+        </div>
+       
+
+              
+
+                <!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+  <div class="child-container">
+            <div class="child1">
+  <section class="container">
+      <form>
+        <div class="form-group">
+        <header class="p-0 mb-3 border-bottom">
+		    <div class="container">
+			    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+			       
+          <div id="header">
+          <h1 class="page-heading">Community<span style="color:gold;">Forum</span></h1>
+          
+			        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-1">
+			       
+			        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" >
+			   
+			    </div>
+		    </div>
+        </div>
+		  </header>
+      
+ <body>
+     
+ <div class="card">
+  <div class="card-body">
+  <div style='text-align:left'>
+  <div class="img">
+    
+    <div style='image-align:left'>
+    <p style="display:inline-block;">
+   
+    <img src="../assets/avatar 1.png" class="brand-logo" alt="">
+  Juanita Dela Ceuz
+</p>
+      <textarea class="form-control status-box" rows="2" placeholder="What's on your mind?"></textarea>
+        </div>
+      </form>
+      <div class="button-group pull-right">
+        <p class="counter"></p>
+      <center>  <a href="#" class="btn btn-primary">Post</a>
+       <a href="#" class="btn btn-primary">Add Photos</a> </center>
+      </div>
+    
+      <ul class="posts">
+      </ul>
+    </div>
+    <br >
+  <div class="card">
+  <div class="card-body">
+  <div style='text-align:left'>
+  <div class="img">
+    
+    <div style='image-align:left'>
+    <p style="display:inline-block;">
+   
+    <img src="../assets/avatar 1.png" class="brand-logo" alt="">
+  Juanita Dela Cruz
+</p>
+ 
+  <div style='text-align:left'>
+    <p class="card-text">Wow!</p>
+ 
+<div class="row">
+  <div class="col-md-4">
+    <div class="img">
+    <img src="../assets/sampleplant.jpg" class="brand-logo" alt="">
+    </div>
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+       
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="img">
+    <img src="../assets/images 2.png" class="brand-logo" alt="">
+    </div>
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+      
+      </div>
+    </div>
+  </div>
+ 
+    </div>
+  </div>
+</div>
+
+        <div class="text-wrapper-6">14</div>
+        <div class="text-wrapper-7">2 comments</div>
+        
+        <input type="submit" name="upvote" value="upvote"> 
+    
+        <input type="submit" name="Comment" value="Comment">
+        
+         <input type="submit" name="Report" value="Report"> 
+
+        
+  </div>
+
+</div>
+
+      </div>
+    </div>
+    <br>
+    <div class="card">
+  <div class="card-body">
+  <div style='text-align:left'>
+  <div class="img">
+    
+    <div style='image-align:left'>
+    <p style="display:inline-block;">
+   
+    <img src="../assets/avatar 1.png" class="brand-logo" alt="">
+  Juanita Dela Cruz
+</p>
+ 
+  <div style='text-align:left'>
+    <p class="card-text">Beautiful</p>
+ 
+<div class="row">
+  <div class="col-md-4">
+    <div class="img">
+    <img src="../assets/images 2.png" class="brand-logo" alt="">
+    </div>
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+       
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="img">
+    <img src="../assets/images 2.png" class="brand-logo" alt="">
+    </div>
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+      
+      </div>
+    </div>
+  </div>
+ 
+    </div>
+  </div>
+</div>
+
+        <div class="text-wrapper-6">14</div>
+        <div class="text-wrapper-7">2 comments</div>
+        
+        <input type="submit" name="upvote" value="upvote"> 
+    
+        <input type="submit" name="Comment" value="Comment">
+        
+         <input type="submit" name="Report" value="Report"> 
+
+        
+  </div>
+
+</div>
+
+      </div>
+    </div>
+  </body>
+</html>
