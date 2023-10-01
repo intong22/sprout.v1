c6e11bfb107f557d679cb0d384c88d0d33d28a91
nesshh <nessatolentino7@gmail.com>
2023-10-01 13:46:08 +0800

    .

--- user/user_forum_profile.php
+++ user/user_forum_profile.php
@@ -29,7 +29,7 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <title> Responsive Sidebar Menu  | CodingLab </title>
+    <title> Community Forum</title>
     <link rel="stylesheet" href="style.css">
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
