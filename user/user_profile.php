9d479ffccb33bd352cb21e99d371bc9ecb364c93
cheopuran <cherrymaeopuran078@gmail.com>
2023-10-16 21:45:55 +0800

    Update user_profile.php

--- user/user_profile.php
+++ user/user_profile.php
@@ -58,10 +58,6 @@
 
 </style>
 </head>
-
-  
-  
-
 <body>
 <div class="sidebar">
     <div class="logo-details">
@@ -145,7 +141,7 @@
         </div>
         <!-- Button to trigger file input -->
         <label for="upload-photo" id="upload-button" class="upload-button">
-            <i class="bx bx-camera"></i> Upload Profile
+            <i class="bx bx-camera"></i> Upload Profile 
         </label>
             <div class="name_job">
               <div class="name"><?php echo $fname." ".$lname; ?></div>
