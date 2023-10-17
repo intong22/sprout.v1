c2a4994fbd830a23cbeb468b51c7bc9ad7e527b8
nesshh <nessatolentino7@gmail.com>
2023-10-17 20:45:45 +0800

    .

--- user/user_plant_tips.php
+++ user/user_plant_tips.php
@@ -15,108 +15,6 @@
 
     <title>Plant Encyclopedia</title>
 
-<style>
-    * {box-sizing:border-box}
-
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
-}
-
-/* Hide the images by default */
-.mySlides {
-  display: none;
-}
-
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  padding: 16px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-  background-color: rgba(0, 0, 0, 0.8); /* Add a background color to make them more visible */
-  z-index: 1; /* Make sure they are above the slides */
-}
-
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-.prev {
-  left: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-.arrow-icon {
-    position: absolute;
-    color: white;
-    top:25px;
-    left: 20px; 
-    font-size: 32px;
-    transform: translateY(-50%);
-}
-
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-
-</style>
 </head>
 <body>
 <div class="sidebar">
@@ -258,38 +156,7 @@
     <h3>Hibiscus</h3>
 
 <!-- Full-width images with number and caption text -->
-    <?php
-      plantImage();
-    ?>
-    
-    <!-- <div class="card-info">
-    <div class="slideshow-container"> -->
-
-    <!-- <div class="mySlides fade">
-      <img src="../assets/hibiscus.jpg" style="width:50%">
-    </div>
-
-    <div class="mySlides fade">
-      <img src="../assets/hibiscus.jpg" style="width:50%">
-    </div>
-
-    <div class="mySlides fade">
-      <img src="../assets/hibiscus.jpg" style="width:50%">
-    </div> -->
-
-    <!-- Next and previous buttons -->
-    <!-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    </div>
-    <br> -->
-
-    <!-- The dots/circles -->
-    <!-- <div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
-    </div> -->
-    <!-- <img src="../assets/hibiscus.jpg" alt="Plant 2" class="modal-image center-image" style="width:100%;max-width:300px;"> -->
+   
     
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </p>
