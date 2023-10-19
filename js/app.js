10f5484f3b97aca25e9e3f8d19be67f5f0e58e67
bontilaoreymart <90241649+bontilaoreymart@users.noreply.github.com>
2023-10-19 22:06:12 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,116 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'PLANT NAME ',
+        item: 'Indoor',
+        image: '../assets/Group 15.jpg',
+        price: 120
+    },
+    {
+        id: 2,
+        name: 'PLANT NAME ',
+        item: 'Indoor',
+        image: '../assets/image 19.jpg',
+        price: 200
+    },
+    {
+        id: 3,
+        name: 'PLANT NAME ',
+        item: 'Indoor',
+        image: '../assets/image 14.jpg',
+        price: 220
+    },
+    {
+        id: 4,
+        name: 'PLANT NAME ',
+        item: 'Indoor',
+        image: '../assets/Group 15.jpg',
+        price: 300
+    },
+    {
+        id: 5,
+        name: 'PLANT NAME ',
+        item: 'Indoor',
+        image: '../assets/image 19.jpg',
+        price: 320
+    },
+    {
+        id: 6,
+        name: 'PLANT NAME ',
+        item: 'Indoor',
+        image: '../assets/image 14.jpg',
+        price: 1200
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="title">${value.item}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.item}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
