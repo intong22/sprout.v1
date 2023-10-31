f7f253cb1ae7ef7ac1625f9ce9c972c6740feaeb
intong <ialexash85@gmail.com>
2023-10-31 19:18:29 +0800

    up

--- backend/bcknd_user_forum_profile.php
+++ backend/bcknd_user_forum_profile.php
@@ -2,6 +2,62 @@
 
     include "connection.php";
 
+    //add comment
+    if(isset($_POST["btnComment"]))
+    {
+        $postID = $_POST["btnComment"];
+        $comment = mysqli_real_escape_string($con, $_POST["inputComment"]);
+
+        //get account ID of poster
+        $getIDQuery = "SELECT
+                            account_id
+                        FROM
+                            user_account
+                        WHERE
+                            account_email = '".$_SESSION["username"]."' ";
+             
+        $id = mysqli_query($con, $getIDQuery);
+ 
+        if(mysqli_num_rows($id) > 0)
+        {
+            $userID = mysqli_fetch_assoc($id);
+            $getID = $userID["account_id"];
+        }
+
+        $commentQuery = "INSERT INTO
+                                    post_comments(account_id, post_id, post_comment)
+                                VALUES
+                                    (".$getID.", ".$postID.", '".$comment."')";
+
+        mysqli_query($con, $commentQuery);
+    }
+
+    //delete post
+    if(isset($_POST["btnDelete"]))
+    {
+        $postID = $_POST["btnDelete"];
+
+        $deletePost = "DELETE FROM
+                            post_information
+                        WHERE
+                            post_id = ".$postID." ";
+        
+        mysqli_query($con, $deletePost);
+    }
+
+    //delete comment
+    if(isset($_POST["delComment"]))
+    {
+        $id = $_POST["delComment"];
+        
+        $del = "DELETE FROM
+                    post_comments
+                WHERE
+                    comment_id = ".$id." ";
+        
+        mysqli_query($con, $del);
+    }
+
     //display user details
     $query = "SELECT 
                     user_account.account_image, user_account.account_firstname, user_account.account_lastname,
@@ -40,114 +96,142 @@
     }
 
     
-    function displayCard()
+    //forum profile card
+    function forumProfile()
     {
         include "connection.php";
-
-        //check if account image is set
-        $account_image_isset = "SELECT
-                                    account_image
-                                FROM
-                                    user_account
-                                WHERE
-                                    account_image IS NOT NULL
-                                AND
-                                    account_email = '".$_SESSION["username"]."' ";
-
-        $results = mysqli_query($con, $account_image_isset);
-
-        if(mysqli_num_rows($results) > 0)
-        {
-            //image is set
-            $flag = true;
-        }
-        else
-        {
-            //image is not set
-            $flag = false;
-        }
-
         //get user posts
         $getPosts = "SELECT 
-                        user_account.account_image, user_account.account_firstname, user_account.account_lastname, 
+                        user_account.account_email, user_account.account_image, user_account.account_firstname, user_account.account_lastname, 
                         post_information.post_id, post_information.post_description, post_information.votes,
-                        post_images_comments.post_image, post_images_comments.post_comment
-                    FROM
+                        MAX(post_images.post_image) AS post_image
+                    FROM 
                         user_account
-                    INNER JOIN
-                        post_information
-                    ON
-                        user_account.account_id = post_information.account_id
-                    LEFT JOIN
-                        post_images_comments
-                    ON 
-                        post_information.post_id = post_images_comments.post_id
+                    INNER JOIN 
+                        post_information ON user_account.account_id = post_information.account_id
+                    LEFT JOIN 
+                        post_images ON post_information.post_id = post_images.post_id
                     WHERE
-                        account_email = '".$_SESSION["username"]."' ";
+                        account_email = '".$_SESSION["username"]."'
+                    GROUP BY 
+                        post_information.post_id ";
         
         $exec = mysqli_query($con, $getPosts);
-
         if(mysqli_num_rows($exec))
         {
             while($populate = mysqli_fetch_assoc($exec))
             {
+                card($populate);
+            }
+        }
+    }
 
-                echo"
-                    <div class='card-body'>
-                    <div style='text-align:left'>
-                        <div class='img'>
+    //card
+    function card($populate)
+    {
+        include "connection.php";       
+
+        echo"<form method='POST' action='user_forum_profile.php'>
+          <div class='container'>
+           <ul class='posts'>";
+
+           if($_SESSION["username"] == $populate["account_email"])
+           {
+                echo "<button type='submit' name='btnDelete' value='".$populate["post_id"]."' style='border: none; float: right;'>Delete post</button>";
+           }
+
+        echo"        <div style='text-align:left'>
+                <div class='profile-image-container'>
+
+                    <div style='image-align:left'>
                     
-                        <div style='image-align:left'>
-                        <p style='display:inline-block;'>";
+                    <p style='display:inline-block;'>";
                         
-                        if($flag == true)
+                        if(!empty($populate["account_image"]))
                         {
-                            echo "<img src='data:image/jpeg;base64,".base64_encode($populate["account_image"])."' alt='User image' class='forum-image' </img>";
+                            echo "<img src='data:image/jpeg;base64,".base64_encode($populate["account_image"])."' alt='User image' class='forum-image'>";
                         }
                         else
                         {
-                            echo "<img src='../assets/user_image_def.png' alt='User image' class='forum-image' </img>";   
+                            echo "<img src='../assets/user_image_def.png' alt='User image' class='forum-image'>";   
                         }
-                echo"
+        echo"
                             ".$populate["account_firstname"]." ".$populate["account_lastname"]."
-                        </p>
-                        
+                    </p>
                             <div style='text-align:left'>
                             <p class='card-text'>".$populate["post_description"]."</p>
                         
                         <div class='row'>
                             <div class='col-md-4'>
-                            <div class='img'>
-                            <img src='data:image/jpeg;base64,".base64_encode($populate["post_image"])."' class='brand-logo' alt='Post image'' </img>
+                                <div class='img'>";
+
+                            if(!empty($populate["post_image"]))
+                            {
+                                echo"<img src='data:image/jpeg;base64,".base64_encode($populate["post_image"])."' class='brand-logo' alt='Post image'>";
+                            }
+                            
+        echo"                   </div>
+                            <div class='card' style='width: 18rem;'>
                             </div>
                         </div>
-                        
-                        </div>
-                    </div>
                     </div>
                     </div>
                 ";
                 echo"
-                    <br>
-                    <div class='text-wrapper-6'> ".$populate["votes"]."
-                        <input type='submit' name='upvote' value='upvote'> 
-                    </div>
-                    <div class='text-wrapper-7'>
-                        <input type='submit' name='Comment' value='Comment'>
-                    </div>
+                    <br>";
+
+        //comments go here
+        //get comments
+        $comments = "SELECT
+                        user_account.account_image, user_account.account_email, user_account.account_firstname, user_account.account_lastname,
+                        post_comments.comment_id, post_comments.account_id, post_comments.post_comment
+                    FROM
+                        user_account
+                    INNER JOIN
+                        post_comments ON post_comments.account_id = user_account.account_id
+                    WHERE
+                        post_id = '".$populate["post_id"]."' ";
+
+        $getComments = mysqli_query($con, $comments);
+
+            if (mysqli_num_rows($getComments) > 0) 
+            {
+                while ($post_comments = mysqli_fetch_assoc($getComments)) 
+                {
+                    if(!empty($post_comments["account_image"]))
+                    {
+                        echo "<p>
+                        <img src='data:image/jpeg;base64,".base64_encode($post_comments["account_image"])."' alt='User image'>";
+                    }
+                    else
+                    {
+                        echo"<img src=../assets/user_image_def.png>";
+                    }
+                    echo $post_comments["account_firstname"]." ".$post_comments["account_lastname"];
+                    echo $post_comments["post_comment"];
                     
-                    <input type='submit' name='Report' value='Report'> 
-                
-                    </div>
-                    </div>
-                    </div>
-                ";
-            }
-        }
-        else
-        {
-            echo "No posts yet.";
-        }
+                    if($_SESSION["username"] == $post_comments["account_email"])
+                    {
+                        echo"<button type='submit' name='delComment' value='".$post_comments["comment_id"]."' style='border: none;'>Delete</button>";
+                    }
+                    echo"</p>";
+                }
+            } 
+        
+
+        echo"                <div class='text-wrapper-6'> ".$populate["votes"]."
+                            <input type='hidden' name='button_value' value='".$populate["post_id"]."'>
+                            <button type='submit' name='upvote' value='upvote'>Upvote</button>
+                        </div>
+                        <div class='text-wrapper-7'>
+                            <input type='text' name='inputComment' placeholder='Comment'>
+                            <button type='submit' name='btnComment'  value='".$populate["post_id"]."'>Comment</button>
+                        </div>  
+                    <button type='submit' name='btnReport' value='".$populate["post_id"]."'>Report</button>
+                    <br><br>
+            </ul>
+            </div>
+            </form>";
     }
 
     //check if account image is set
