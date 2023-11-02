a875c6e03dc7c8abeb8c14fc4e5047d9066a21f7
nesshh <nessatolentino7@gmail.com>
2023-11-02 18:34:42 +0800

    marketplace_profile

---
+++ user/user_marketplace_profile.php
@@ -0,0 +1,295 @@
+<?php
+    include "../backend/session_logged_in.php";
+    include "../backend/bcknd_user_homepage.php";
+    include "../backend/bcknd_user_profile.php";
+?>
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>User Homepage</title>
+    <link rel="stylesheet" href="../css/user_sidebar.css">
+    <link rel="stylesheet" href="../css/user_homepage.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  position: relative;
+  background-color: #fefefe;
+  margin: auto;
+  padding: 0;
+  border: 1px solid #888;
+  width: 80%;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+  -webkit-animation-name: animatetop;
+  -webkit-animation-duration: 0.4s;
+  animation-name: animatetop;
+  animation-duration: 0.4s
+}
+
+/* Add Animation */
+@-webkit-keyframes animatetop {
+  from {top:-300px; opacity:0} 
+  to {top:0; opacity:1}
+}
+
+@keyframes animatetop {
+  from {top:-300px; opacity:0}
+  to {top:0; opacity:1}
+}
+
+/* The Close Button */
+.close {
+  color: white;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.modal-header {
+  padding: 2px 16px;
+  background-color: #1E5631;
+  color: white;
+}
+
+.modal-body {padding: 2px 16px;}
+
+.modal-footer {
+  padding: 2px 16px;
+  background-color: #1E5631;
+  color: white;
+}
+</style>
+   </head>
+   
+<body>
+  <!--SIDEBAR-->
+  <div class="sidebar">
+      <div class="logo-details">
+        <img src="..\assets\logo.png" alt="Logo" class="logo-details">
+        <i class='bx bx-menu' id="btn" > </i>         
+      <div class="logo_name">Sprout</div>  
+      </div>
+      <ul class="nav-list">
+    <li>
+      <a href="user_homepage.php">
+        <i class='bx bx-home' ></i>
+        <span class="links_name">Home</span>
+      </a>
+      <span class="tooltip">HOME</span>
+      </li>
+    <li>
+    <a href="user_encyclopedia.php">
+      <i class='bx bx-book-open' ></i>
+        <span class="links_name">Encyclopedia</span>
+    </a>
+      <span class="tooltip">ENCYCLOPEDIA</span>
+    </li>
+    <li>
+    <a href="user_forum.php">
+      <i class='bx bx-chat' ></i>
+      <span class="links_name">Community Forum</span>
+    </a>
+    <span class="tooltip">COMMUNITY FORUM</span>
+    </li>
+    <li>
+    <a href="user_marketplace.php">
+      <i class='bx bx-folder' ></i>
+      <span class="links_name">Marketplace</span>
+    </a>
+      <span class="tooltip">MARKETPLACE</span>
+    </li>
+    <li>
+    <a href="user_bookmark.php">
+      <i class='bx bx-book-bookmark' ></i>
+      <span class="links_name">Bookmark</span>
+    </a>
+      <span class="tooltip">BOOKMARK</span>
+    </li>
+    <li>
+    <a href="user_like.php">
+      <i class='bx bxs-cart-add' ></i>
+        <span class="links_name">Cart</span>
+    </a>
+      <span class="tooltip">CART</span>
+    </li>
+    <li>
+    <a href="user_profile.php">
+    <i class='bx bx-user' ></i>
+      <span class="links_name">User</span>
+    </a>
+    <span class="tooltip">USER PROFILE</span>
+    </li>
+    <li>
+    <a href="user_subscription.php">
+    <i class='bx bx-dollar' ></i>
+      <span class="links_name">Subscription</span>
+    </a>
+    <span class="tooltip">Subscription</span>
+    </li>
+     <li class="profile">
+         <div class="profile-details">
+         <div class="profile-image-container" onclick="toggleUploadButton()">
+            <?php 
+                if($flag == true)
+                {
+                  echo $image; 
+                }
+                else
+                {
+                  echo "<img src='../assets/user_image_def.png' alt='User image' class='user-image' </img>";   
+                } 
+            ?> 
+        </div>
+            <div class="name_job">
+              <div class="name"><?php echo $fname." ".$lname; ?></div>
+              <div class="job"><?php echo $status; ?></div>
+            </div>
+        </div>
+		   <a href="../backend/session_end.php">
+         <i class='bx bx-log-out' id="log_out" ></i>
+		 </a>
+		   <span class="tooltip">LOGOUT</span>
+     </li>
+    </ul>
+  </div>
+
+  <script src="../js/homepage.js"></script>	
+ 
+
+  <section class="home-section">
+    <!-- <header style="padding:20px;">
+    <a href="user_homepage.php" style="text-decoration: none">
+      <h1 class="colored-text"> <span class="white">S p r</span><span class="orange"> o u t</span> </h1><br>
+    </a>
+    <form method="GET" action="user_homepage.php">
+            <input name="searchInput" class="search-input" type="text" placeholder="Search...">
+            <button name="btnSearch" class="search-button" type="submit">Search</button>
+    </form>
+       
+    <br>    
+    </header> -->
+    <button id="myBtn">Create Post</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">&times;</span>
+      <h2>Marketplace</h2>
+    </div>
+    <div class="modal-body">
+    <form action="admin_create_encyclopedia.php" method="POST" enctype="multipart/form-data">
+                <label for="plant_name">PLANT NAME:</label>
+                <input type="text" id="plant_name" name="plant_name" required><br><br>
+               
+                <label for="plant_name">PLANT TYPE:</label>
+                <select id="plant_type" name="plant_type" required>
+                    <option value="flowering">Flowering</option>
+                    <option value="s&c">Succulents & Cacti</option>
+                    <option value="fern">Fern</option>
+                    <option value="climber">Climbers</option>
+                    <option value="fruit">Fruit Bearing</option>
+                    <option value="vegetable">Vegetable Bearing</option>
+                    <option value="herbal">Herbal</option>
+                    <option value="fungi">Fungi</option>
+                    <option value="carnivorous">Carnivorous</option>
+                    <option value="toxic">Toxic</option>
+                    <option value="onramental">Ornamental</option>
+                </select><br><br>
+                
+
+                <label for="description">Description:</label><br>
+                <textarea id="description" name="description" rows="4" cols="50" required></textarea><br><br>
+
+                <label for="price">Price:</label><br>
+                <input type="number" name="price" rows="4" cols="50" required></textarea><br><br>
+
+
+                <label for="image_url">Image URL:</label>
+                <input type="file" id="image_url" name="plant_image[]" accept=".jpg, .png, .jpeg" multiple required>
+                <br><br>
+
+                <button name="btnSubmit" class="button">Submit</button>   
+                
+            </form>   
+    </div>
+    <div class="modal-footer">
+     <h3>Marketplace</h3>
+    </div>
+  </div>
+
+</div>
+    
+</div>
+    </div>
+  
+    <br>
+ 
+  
+  </section>
+ 
+
+  <script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+</body>
+
+</html>
+
+
