a991b15d1a97653a6a6aa2cd3b5602257e6c9c69
intong <ialexash85@gmail.com>
2023-12-02 00:16:11 +0800

    Update admin_home.php

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -57,7 +57,7 @@
       ?>
     </div>
   </section>
-  <div class="modal fade" id="homeModal" tabindex="-1" role="dialog" aria-labelledby="homeModalLabel" aria-hidden="true">
+<div class="modal fade" id="homeModal" tabindex="-1" role="dialog" aria-labelledby="homeModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -116,6 +116,7 @@
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <script>
         Swal.fire({
