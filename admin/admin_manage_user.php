26f35317fd3e7788211c81d95526462dd63b4cae
intong <ialexash85@gmail.com>
2023-12-06 00:36:31 +0800

    Update admin_manage_user.php

--- admin/admin_manage_user.php
+++ admin/admin_manage_user.php
@@ -5,7 +5,7 @@
   include "admin_sidebar.php";
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
